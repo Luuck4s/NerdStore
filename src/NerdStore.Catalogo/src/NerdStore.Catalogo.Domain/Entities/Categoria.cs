b5848f8b699581aac9b4166f46d78bd4a3d709ff
Lucas Andrade de Lima <lucass.limaa@outlook.com.br>
2022-11-28 21:26:30 -0300

    feat: adding base entities

---
+++ src/NerdStore.Catalogo/src/NerdStore.Catalogo.Domain/Entities/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using NerdStore.Core.Entities;
+
+namespace NerdStore.Catalogo.Domain.Entities;
+
+public class Categoria: Entity
+{
+    public string Nome { get; private set; }
+
+    public int Codigo { get; private set; }
+
+    public Categoria(string nome, int codigo)
+    {
+        Nome = nome;
+        Codigo = codigo;
+    }
+
+    public override void Validar()
+    { }
+
+    public override string ToString()
+    {
+        return $"{Nome} - {Codigo}";
+    }
+}
---
+++ src/NerdStore.Core/src/NerdStore.Core/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,6 @@
+﻿namespace NerdStore.Core.Interfaces;
+
+public interface IAggregateRoot
+{
+    
+}
