5ee0787e731b58262ce17f7b7c5560e665237b05
Lucas Andrade de Lima <lucass.limaa@outlook.com.br>
2022-12-09 08:39:08 -0300

    feat: add domain service

---
+++ src/NerdStore.Catalogo/src/NerdStore.Catalogo.Domain/Services/IEstoqueService.cs
@@ -0,0 +1,7 @@
+﻿namespace NerdStore.Catalogo.Domain.Services;
+
+public interface IEstoqueService: IDisposable
+{
+    Task<bool> DebitarEstoque(Guid produtoId, int quantidade);
+    Task<bool> ReporEstoque(Guid produtoId, int quantidade);
+}
